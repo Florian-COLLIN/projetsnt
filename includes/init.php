21d7b9e2489c5352b164870c5c87d94008b34a4c
nathan cerisara <cerisara.nathan@gmail.com>
2020-11-15 11:35:46 +0100

    change init.php

--- includes/init.php
+++ includes/init.php
@@ -1,12 +1,31 @@
 <?php
 session_start();
 
+
+$file_path = "mysql-config.json";
+
+function open_json($file_path){
+    if( file_exists($file_path) ){
+        $texte = file_get_contents($file_path);
+        $data = json_decode($texte, true);
+    }
+    else{
+        $data=[];
+    }
+    return $data;
+}
+
+$data_account = open_base($file_path)
+$pseudo = $data_account["pseudo"];
+$password = $data_account["password"];
+$db_name = $data_account["db-name"];
+
 if (isset($_COOKIE["logged"]) and $_COOKIE["logged"] = true) {
 	$_SESSION["logged"] = true;
 }
 
 try {
-	$db = new PDO("mysql:host=localhost;dbname=le-point-k;charset=utf8", "root", "");
+	$db = new PDO("mysql:host=localhost;dbname=".$db_name.";charset=utf8", $pseudo, $password);
 }
 
 catch(Exception $e) {
