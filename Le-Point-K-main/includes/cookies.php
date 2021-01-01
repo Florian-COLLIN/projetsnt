4c347702a2e12af533fbe35c2ddee71a515432e4
AirSThib <75171564+AirSThib@users.noreply.github.com>
2021-01-01 16:17:25 +0100

    Update files

--- Le-Point-K-main/includes/cookies.php
+++
@@ -1,1 +0,0 @@
-<i class="fas fa-cookie-bite" data-toggle="tooltip" title="Cette fonctionnalité nécessite des cookies"> </i>
--- Le-Point-K-main/includes/message.php
+++
@@ -1,6 +0,0 @@
-/* FILE TO INCLUDE MESSAGE */
-
-switch($_SESSION["message"]) {
-  case "welcome":
-    /* Code of a message */
-}
